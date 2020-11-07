a1ee76630fe20ce55eb50141f7f13ad1aa879085
Ricardo Salinas <ricardo.salinas@butterfly.com.au>
2020-11-08 09:11:13 +1100

    Restructure

--- api/rest-api.py
+++ api/rest-api.py
@@ -1,16 +1,37 @@
 import flask
 import flask_sqlalchemy
 import flask_restless
-from models import *
-from sqlalchemy import Column, Float, Integer, String, Date, Enum
+from sqlalchemy import Column, Float, Integer, String, Date, Enum, Unicode
 import enum
 import os
+from flask_jwt import JWT, jwt_required, current_identity
+
+DATABASE = os.environ.get('DATABASE','sqlite:////tmp/moneyman.sqlite')
 
 # Create the Flask application and the Flask-SQLAlchemy object.
-app = flask.Flask(__name__,static_folder="../static/dist")
+app = flask.Flask(__name__,static_folder="./static/dist")
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE','sqlite:///moneyman.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config["JWT_SECRET_KEY"] = "6174AF5FAD1CAF4E7558DB85343FEC509CDC8C719FD6D9DD57329EF9A7D1BB51"
 db = flask_sqlalchemy.SQLAlchemy(app)
+
+class User(object):
+
+    def __init__(self, id):
+        self.id = str(id)
+
+def authenticate(username, password):
+    return User(1)
+
+def identity(payload):
+    return User(1)
+
+@jwt_required()
+def auth_func(**kw):
+    pass
+
+jwt = JWT(app, authenticate, identity)
+
 
 # Create your Flask-SQLALchemy models as usual but with the following two
 # (reasonable) restrictions:
@@ -79,7 +100,8 @@ from sqlalchemy import event
 import hashlib
 @event.listens_for(Transfer, 'before_insert')
 def receive_before_insert(mapper, connection, target):
-    target.ref = hashlib.md5("%s %s %s".format(target.date, target.account, target.description).encode('utf-8')).hexdigest()
+    ref = f"{target.date} {target.account} {target.description}".encode('utf-8')
+    target.ref = hashlib.md5(ref).hexdigest()
 
 # Create the database tables.
 db.create_all()
@@ -87,13 +109,14 @@ db.create_all()
 # Create the Flask-Restless API manager.
 manager = flask_restless.APIManager(app, flask_sqlalchemy_db=db)
 
+jwt_required=dict(GET_SINGLE=[auth_func], GET_MANY=[auth_func])
+
 # Create API endpoints, which will be available at /api/<tablename> by
 # default. Allowed HTTP methods can be specified as well.
 manager.create_api(Transfer, methods=['GET', 'POST'])
 manager.create_api(Payment, methods=['GET', 'POST', 'PUT', 'DELETE'])
-manager.create_api(Plan, methods=['GET', 'POST'])
+manager.create_api(Plan, methods=['GET', 'POST', 'PUT', 'DELETE'])
 manager.create_api(Account, methods=['GET', 'POST'])
-
 
 # manager.create_api(Computer, methods=['GET'])
 
@@ -103,4 +126,19 @@ manager.create_api(Account, methods=['GET', 'POST'])
 def react():
     return flask.render_template("react.html")
 
-app.run()
+@app.route('/delete')
+def remove():
+    os.remove(DATABASE[10:])
+    return "Deleted"
+
+@app.route('/create')
+def create():
+    db.create_all()
+    return "Created"
+
+@app.route('/globals')
+def show_globals():
+    return {"DATABASE": DATABASE}
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=os.environ.get("PORT", 5000))

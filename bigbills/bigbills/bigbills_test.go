f9719f1829b306db5450af7c0d022502a45d1a41
Ricardo Salinas <ricardo.salinas@altavec.com>
2022-12-08 23:56:19 +1100

    (cleanup) Remove unused var

--- bigbills/bigbills/bigbills_test.go
+++ bigbills/bigbills/bigbills_test.go
@@ -16,30 +16,6 @@ import (
 	"google.golang.org/api/sheets/v4"
 )
 
-var test_config_yaml = []byte(`---
-sid: 00000000000
-token: 00000000000
-spreadsheet_id: testspreadsheetid
-spreadsheet_range: "Big Bills!M2:P"
-account_id: "000000"
-backend: https://fake.com/api/transactions
-mobiles:
-  - "+6140000000"
-credentials: |
-  {
-      "type": "service_account",
-      "project_id": "project-1234567890123",
-      "private_key_id": "radacted",
-      "private_key": "-----BEGIN PRIVATE KEY-----\nP01crPCOMbZ4krHR4qmrpTAAuiVKguaQ7zxRBitExfRj4kHSJ6pDgml4UTdpBQ+A\nYVWxjwIDAQABAkEAlbQZJc83FsH4Fut356jEmC1EFXpYyfv5mgPBz4YyD0JfTz/L\noMEQZExt2dNLLSftCUCEEhTgVn9KhNE7AbkHwQIhAPMV6EKa3MEjbnQ50hz3zMWR\nmtmqdyvlIAzEAN/3nv1hAiEA2+Xt14J9UIOtI5bYMNkEQGukXl8f+vksBTHlVbS1\npO8CIHFY8r8z9OG+Qr/BQl8tkPdDCMCcQwtdwI8TShElJqahAiBBtRf34K8PYvlW\nfBmHpyFFGqUX6fbFaLVFrB4qGQB6EwIhAJ/Kib37tH2BumOPxu+gP/KJsIOAKdXX\nUN0yFBuXahyi\n-----END PRIVATE KEY-----",
-      "client_email": "sauser@project-1234567890123.iam.gserviceaccount.com",
-      "client_id": "0000000000000000000",
-      "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-      "token_uri": "https://oauth2.googleapis.com/token",
-      "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-      "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/sauser%40project-1234567890123.iam.gserviceaccount.com"
-  }
-`)
-
 type MockRoundTripper func(req *http.Request) (res *http.Response, err error)
 
 func (m MockRoundTripper) RoundTrip(req *http.Request) (res *http.Response, err error) {

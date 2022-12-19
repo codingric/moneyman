19c1cbdcff1402bcced96ed83beb046dbc0788b8
Ricardo Salinas <ricardo.salinas@altavec.com>
2022-12-19 21:45:26 +1100

    (add) Backend logging the transaction verb

--- backend/setup.go
+++ backend/setup.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"github.com/rs/zerolog"
@@ -34,7 +35,8 @@ func (z zerologger) Warn(c context.Context, m string, x ...interface{})  { z.Log
 func (z zerologger) Error(c context.Context, m string, x ...interface{}) { z.Logger.Error().Msg(m) }
 func (z zerologger) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
 	s, r := fc()
-	z.Logger.Trace().Int64("rows", r).Dur("duration_ms", time.Since(begin)).Msg(s)
+	verb := strings.ToLower(strings.Split(s, " ")[0])
+	z.Logger.Trace().Int64("rows", r).Dur("duration_ms", time.Since(begin)).Str("verb", verb).Msg(s)
 }
 
 func ConnectDatabase(path string, debug bool) {

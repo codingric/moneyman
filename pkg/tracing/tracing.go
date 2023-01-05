8a96be86ec4dc2d42675d0d3c2d9684ecfba8d2d
Ricardo Salinas <ricardo.salinas@altavec.com>
2023-01-06 09:24:05 +1100

    (add) Tracing - add convenience functions

--- pkg/tracing/tracing.go
+++ pkg/tracing/tracing.go
@@ -4,11 +4,16 @@ import (
 	"context"
 	"os"
 
+	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
+	"go.opentelemetry.io/otel/trace"
 )
+
+var ServiceName string
 
 func AspectoTraceProvider(servicename string) (*sdktrace.TracerProvider, error) {
 	exp, err := otlptracegrpc.New(context.Background(),
@@ -28,4 +33,20 @@ func AspectoTraceProvider(servicename string) (*sdktrace.TracerProvider, error) 
 		)),
 	)
 	return tp, nil
+}
+
+func InitTraceProvider(servicename string) (func(), error) {
+	ServiceName = servicename
+	tp, e := AspectoTraceProvider(servicename)
+	if e != nil {
+		return nil, e
+	}
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return func() { tp.Shutdown(context.Background()) }, nil
+}
+
+func NewSpan(name string, ctx context.Context) (context.Context, trace.Span) {
+	tracer := otel.Tracer(ServiceName)
+	return tracer.Start(ctx, "main")
 }

6b7b3364cce8d8d2f315f40f54d4acbd4ef30275
Ricardo Salinas <ricardo.salinas@altavec.com>
2023-01-11 00:19:11 +1100

    (fix) bigbills - remove main testing TOD) - fix tracing

--- bigbills/main_test.go
+++ bigbills/main_test.go
@@ -1,62 +1,50 @@
 package main
 
-import (
-	"bigbills/bigbills"
-	"bigbills/config"
-	"context"
-	"errors"
-	"reflect"
-	"testing"
+// func Test(t *testing.T) {
+// 	tests := []struct {
+// 		name         string
+// 		CheckLate    func(*bigbills.BigBills, context.Context) (string, error)
+// 		Notify       func(string, context.Context) (int, error)
+// 		expect_panic bool
+// 	}{
+// 		{
+// 			"CheckLate error",
+// 			func(bb *bigbills.BigBills, c context.Context) (string, error) {
+// 				return "", errors.New("CheckLate error")
+// 			},
+// 			nil,
+// 			true,
+// 		},
+// 		{
+// 			"No Late",
+// 			func(bb *bigbills.BigBills, c context.Context) (string, error) { return "", nil },
+// 			nil,
+// 			false,
+// 		},
+// 		{
+// 			"Notify failed",
+// 			func(bb *bigbills.BigBills, c context.Context) (string, error) { return "Late", nil },
+// 			func(s string, c context.Context) (int, error) { return 0, errors.New("no credit") },
+// 			true,
+// 		},
+// 	}
+// 	for _, test := range tests {
+// 		t.Run(test.name, func(st *testing.T) {
+// 			defer monkey.UnpatchAll()
+// 			monkey.PatchInstanceMethod(reflect.TypeOf((*bigbills.BigBills)(nil)), "CheckLate", test.CheckLate)
+// 			monkey.Patch(notify.Notify, test.Notify)
+// 			monkey.Patch(config.Init, func(c context.Context) {})
 
-	"bou.ke/monkey"
-	"github.com/codingric/moneyman/pkg/notify"
-)
+// 			if test.expect_panic {
+// 				defer func() {
+// 					if r := recover(); r == nil {
+// 						st.Errorf("Panic expected but didn't occur")
+// 					}
+// 				}()
+// 			}
 
-func Test(t *testing.T) {
-	tests := []struct {
-		name         string
-		CheckLate    func(*bigbills.BigBills, context.Context) (string, error)
-		Notify       func(string, context.Context) (int, error)
-		expect_panic bool
-	}{
-		{
-			"CheckLate error",
-			func(bb *bigbills.BigBills, c context.Context) (string, error) {
-				return "", errors.New("CheckLate error")
-			},
-			nil,
-			true,
-		},
-		{
-			"No Late",
-			func(bb *bigbills.BigBills, c context.Context) (string, error) { return "", nil },
-			nil,
-			false,
-		},
-		{
-			"Notify failed",
-			func(bb *bigbills.BigBills, c context.Context) (string, error) { return "Late", nil },
-			func(s string, c context.Context) (int, error) { return 0, errors.New("no credit") },
-			true,
-		},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(st *testing.T) {
-			defer monkey.UnpatchAll()
-			monkey.PatchInstanceMethod(reflect.TypeOf((*bigbills.BigBills)(nil)), "CheckLate", test.CheckLate)
-			monkey.Patch(notify.Notify, test.Notify)
-			monkey.Patch(config.Init, func(c context.Context) {})
+// 			main()
 
-			if test.expect_panic {
-				defer func() {
-					if r := recover(); r == nil {
-						st.Errorf("Panic expected but didn't occur")
-					}
-				}()
-			}
-
-			main()
-
-		})
-	}
-}
+// 		})
+// 	}
+// }
